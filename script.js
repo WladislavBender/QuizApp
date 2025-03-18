07794e8164db357a3277904c52a56df68c0e03af
Wladislav Bender <wladislav.bender@gmail.com>
2025-03-18 21:26:00 +0100

    added style and data

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist ein Algorithmus?",
+        "answer_1": "Eine Programmiersprache",
+        "answer_2": "Eine Folge von Anweisungen zur Lösung eines Problems",
+        "answer_3": "Ein Hardware-Bauteil",
+        "answer_4": "Ein Software-Entwicklungswerkzeug",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist eine Variable in der Programmierung?",
+        "answer_1": "Ein Datentyp für Zahlen",
+        "answer_2": "Ein Speicherplatz für Daten mit einem bestimmten Namen",
+        "answer_3": "Eine Funktion zur Datenmanipulation",
+        "answer_4": "Ein Befehl zur Programmausführung",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet 'Debugging'?",
+        "answer_1": "Das Schreiben von Code",
+        "answer_2": "Das Testen von Code",
+        "answer_3": "Das Entfernen von Fehlern im Code",
+        "answer_4": "Das Optimieren von Code",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist eine 'Schleife' in der Programmierung?",
+        "answer_1": "Eine Funktion zur Datenmanipulation",
+        "answer_2": "Ein Befehl zur Programmausführung",
+        "answer_3": "Eine Kontrollstruktur, die es ermöglicht, einen Codeblock wiederholt auszuführen",
+        "answer_4": "Ein Datentyp für Zeichenketten",
+        "right_answer": 3
+    },
+];
