9297bd3756ee4e19a9508227511ea1f9bca6dab5
Wladislav Bender <wladislav.bender@gmail.com>
2025-03-19 19:59:15 +0100

    added some js

--- script.js
+++ script.js
@@ -40,3 +40,37 @@ let questions = [
         "right_answer": 3
     },
 ];
+
+let currentQuestion = 0;
+
+
+function init() {
+    document.getElementById('allQuestions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    if (selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    }
+    else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+    }
+
+}
